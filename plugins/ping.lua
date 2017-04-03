054e81175ccf13e15a4ae12d726c7f16e9a0207d
arashvp1 <arashvp5@gmail.com>
2017-04-03 04:34:43 -0700

    Add files via upload

---
+++ plugins/ping.lua
@@ -0,0 +1,17 @@
+﻿--start by edit by @avbat
+local datebase = {
+  "  من نباشم کی باشه؟😐❤️ ",
+
+  }
+local function run(msg, matches) 
+return datebase[math.random(#datebase)]
+end
+return {
+  patterns = {
+    "^(انلاینی)",
+  },
+  run = run
+}
+
+--end by edit by @avbat
+--Channel 
