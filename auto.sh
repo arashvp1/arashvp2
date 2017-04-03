b78b6f4b794f6cd2bf80bed5ce3c99428ec65455
arashvp1 <arashvp5@gmail.com>
2017-04-03 04:40:36 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,6 +2,6 @@
 COUNTER=1
 while(true) do
 ./parsol.sh
-curl "https://api.telegram.org/bot[ToKeN]/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-Reloaded-${COUNTER}-times"
+curl "https://api.telegram.org/bot375680929:AAHwDKcT4CLSOasmIBWCk-6UQ6_zt2obLlQ/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
